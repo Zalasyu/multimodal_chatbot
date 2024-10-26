9729b8fcab93374c3cb15051db73d500850eb4fa
Zalasyu <alec_moldovan@yahoo.com>
2024-10-26 20:58:21 +0100

    Create some placeholder classes for next components: preprocessors and embedding.

---
+++ emedding/emedding_hub.py
@@ -0,0 +1,14 @@
+class TextEmbedder:
+    def __init__(self, model):
+        self.model = model
+
+    def embed(self, text):
+        pass
+
+
+class MultiModalEmbedder:
+    def __init__(self, model):
+        self.model = model
+
+    def embed(self, text):
+        pass
--- preprocess/preprocessor.py
+++ preprocess/preprocessor.py
@@ -1,3 +1,4 @@
+# TODO: Create method to extract frames and metadata and create image-text pairs to pass embedding model later.
 class Preprocessor:
     def __init__(self):
         pass
